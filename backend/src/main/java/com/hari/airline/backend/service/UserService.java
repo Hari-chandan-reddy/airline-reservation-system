63a6765f00525880aa261c866f14fac28c17cd11
Pasula Hari Chandan Reddy <pharichandankkvs@gmail.com>
2026-06-07 09:17:52 +0530

    feat(user-service): add registerUser method

---
+++ backend/src/main/java/com/hari/airline/backend/service/UserService.java
@@ -0,0 +1,29 @@
+package com.hari.airline.backend.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.hari.airline.backend.entity.User;
+import com.hari.airline.backend.repository.UserRepository;
+
+@Service
+public class UserService {
+	private final UserRepository userRepository;
+	
+	// Constructor Injection (Dependency injection)
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	// Business Logic: Register a new user
+	public User registerUser(User user) {
+		// Business Rule: Check if email is already taken
+		Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
+		if(existingUser.isEmpty()) {
+			throw new RuntimeException("Email is already registered!");
+		}
+		
+		return userRepository.save(user);
+	}
+}
